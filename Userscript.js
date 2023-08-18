1f4cb8116f1558a8539840478362bc77af48e79d
Kumar Satyam <110342305+krsatyam7@users.noreply.github.com>
2023-08-18 11:36:46 +0530

    Update and rename New Userscript.user.js to Userscript.js

--- Userscript.js
+++ Userscript.js
@@ -5,20 +5,20 @@
 // @description  Are you still skipping classes??
 // @author       krsatyam7
 // @match        https://niet.instituteoncloud.com/AttendanceReport/StudentAcademicAttendance
-// @icon         https://github.com/krsatyam7/NIET-Attendance-Viewer/blob/main/icons.png
-// @grant        none
+// @icon         https://user-images.githubusercontent.com/110342305/230463675-b0cde796-e67d-4330-a53d-e3530b9ab1da.png
+// @license MIT 
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     // Your code here...
     // Get all elements with the "display:none" property
 const hiddenElements = document.querySelectorAll("[style*='display:none']");
-
+ 
 // Loop through each hidden element and remove its "display:none" property
 hiddenElements.forEach(function(element) {
   element.style.display = "";
 });
-
+ 
 })();
